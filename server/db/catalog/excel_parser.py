a3b2c70e3cdd167c1892b068bf936b7073a77023
Nam Tran <namtran211101@gmail.com>
2024-04-07 21:00:37 +1000

    fix datatype corresponding to fields

--- server/db/catalog/excel_parser.py
+++ server/db/catalog/excel_parser.py
@@ -1,14 +1,15 @@
-import pandas as pd
-import numpy as np
-import os
 import json
+import os
 
-file_path = os.path.join( os.getcwd(), 'fsaea-docs', 'BOM Catalog.xlsx' )
+import numpy as np
+import pandas as pd
+
+file_path = os.path.join(os.getcwd(), 'fsaea-docs', 'BOM Catalog.xlsx')
 
 # Load the Excel file
 excel_file = pd.ExcelFile(file_path)
 # Get the sheet names
-sheet_names = excel_file.sheet_names[1:] # skip first page
+sheet_names = excel_file.sheet_names[1:]  # skip first page
 
 # Iterate over each sheet
 for sheet_name in sheet_names:
@@ -18,10 +19,10 @@ for sheet_name in sheet_names:
     sheet = sheet.iloc[:, 1:]
     # Replace NaN values with null
     sheet = sheet.where(pd.notnull(sheet), "None")
-    
+
     # Specify the output JSON file path
-    
-    json_file = 'server/db/catalog/'+f'{sheet_name.lower()}.json'
+
+    json_file = 'server/db/catalog/' + f'{sheet_name.lower()}.json'
     # List to store JSON objects
     json_objects = []
 
@@ -29,12 +30,19 @@ for sheet_name in sheet_names:
     for _, row in sheet.iterrows():
 
         row_dict = row.to_dict()
-        formatted_dict = {key.replace('[', '').replace(']', ''): str(value).replace('[', '').replace(']', '') for key, value in row_dict.items()}
+        formatted_dict = {key.replace('[', '').replace(']', ''): value for key, value in row_dict.items()}
 
-        if "Cost" in formatted_dict:
-            if formatted_dict["Cost"] == "Needs Calculation" or formatted_dict["Cost"] == "Needs Calc":
-                formatted_dict["Cost"] = "None"
-            # Append the dictionary to the list
+        # remove [] in key, remove none with null and remove needs calc
+        for key, value in formatted_dict.items():
+            if isinstance(value, str):
+                formatted_dict[key] = str(value).replace('[', '').replace(']', '')
+            if value == "None" or value == "[Needs Calculation]" or value == "[Needs Calc]":
+                formatted_dict[key] = None
+
+        # if "Cost" in formatted_dict:
+        #     if formatted_dict["Cost"] == "Needs Calculation" or formatted_dict["Cost"] == "Needs Calc":
+        #         formatted_dict["Cost"] = "None"
+        #     # Append the dictionary to the list
         json_objects.append(formatted_dict)
 
     # Write JSON objects to a JSON file
